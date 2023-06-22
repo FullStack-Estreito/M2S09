cffcd4bc941a90c1937ff7ac84ede6b174776188
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-22 21:02:25 +0000

    Base aula 02

--- aula-02/src/app/app.module.ts
+++ aula-02/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SerieComponent } from './components/serie/serie.component';
+import { ListaEpisodiosComponent } from './components/lista-episodios/lista-episodios.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SerieComponent,
+    ListaEpisodiosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
