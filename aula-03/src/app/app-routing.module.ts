5dedbf342e504e82a6813550fe5c509ddf4fdbbf
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-25 00:56:14 +0000

    Base aula 3

--- aula-03/src/app/app-routing.module.ts
+++ aula-03/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/private/home/home.component';
+import { SerieComponent } from './components/private/serie/serie.component';
+import { FullComponent } from './layouts/full/full.component';
+import { NotFoundComponent } from './components/public/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/content/home', pathMatch: 'full' },
+  { 
+  path: 'content', component: FullComponent,
+  children: [
+    { path: 'home', component: HomeComponent },
+    { path: 'serie/:id', component: SerieComponent },
+    { path: '**', component: NotFoundComponent }
+  ] 
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- aula-03/src/app/app.module.ts
+++ aula-03/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/private/home/home.component';
+import { SerieComponent } from './components/private/serie/serie.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { FullComponent } from './layouts/full/full.component';
+import { NotFoundComponent } from './components/public/not-found/not-found.component';
+import { SigninComponent } from './components/public/signin/signin.component';
+import { SignupComponent } from './components/public/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SerieComponent,
+    HeaderComponent,
+    FooterComponent,
+    FullComponent,
+    NotFoundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
