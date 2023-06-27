727ac775e9c199a82395da8e175c72f31f262d48
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-27 01:07:11 +0000

    Conteúdo aula 1

--- aula-01/src/app/app.module.ts
+++ aula-01/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemploLifecycleComponent } from './components/exemplo-lifecycle/exemplo-lifecycle.component';
+import { SerieComponent } from './components/serie/serie.component';
+import { ListaEpisodiosComponent } from './components/lista-episodios/lista-episodios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemploLifecycleComponent
+    ExemploLifecycleComponent,
+    SerieComponent,
+    ListaEpisodiosComponent
   ],
   imports: [
     BrowserModule,
