4bb08a4bb44d719af55f5ac3708e7be7f699c35d
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-29 01:05:49 +0000

    Conteudo aula 3

--- aula-03/src/app/app-routing.module.ts
+++ aula-03/src/app/app-routing.module.ts
@@ -10,9 +10,15 @@ const routes: Routes = [
     path: '',
     component: FullComponent,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'serie/:id', component: SerieComponent },
+      { path: '', redirectTo: 'private/home', pathMatch: 'full' },
+      { 
+        path: 'private',
+        loadChildren: () => import('./components/private/private.module').then(m => m.PrivateModule)
+      },
+      { 
+        path: 'public',
+        loadChildren: () => import('./components/public/public.module').then(m => m.PublicModule)
+      },
       { path: '**', component: NotFoundComponent }
     ]
   }
--- aula-03/src/app/app.module.ts
+++ aula-03/src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/private/home/home.component';
-import { SerieComponent } from './components/private/serie/serie.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 import { FullComponent } from './layouts/full/full.component';
 import { NotFoundComponent } from './components/public/not-found/not-found.component';
-import { SigninComponent } from './components/public/signin/signin.component';
-import { SignupComponent } from './components/public/signup/signup.component';
+import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SerieComponent,
-    HeaderComponent,
-    FooterComponent,
     FullComponent,
-    NotFoundComponent,
-    SigninComponent,
-    SignupComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
